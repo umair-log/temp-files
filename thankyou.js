68dfc684f33019be536923a1eb231a7697c6a784
umair-log <buttjeefull77@gmail.com>
2025-06-05 22:15:19 +0500

    pixel add

--- thankyou.js
+++ thankyou.js
@@ -16,6 +16,30 @@ export default function ThankYou() {
         />
       </Head>
 
+      <Script id="fb-pixel-init" strategy="afterInteractive">
+      {`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '722676669519092');
+        fbq('track', 'PageView');
+      `}
+    </Script>
+    <noscript>
+      <img
+        height="1"
+        width="1"
+        style={{ display: 'none' }}
+        src="https://www.facebook.com/tr?id=722676669519092&ev=PageView&noscript=1"
+      />
+    </noscript>
+
+
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
@@ -61,3 +85,4 @@ export default function ThankYou() {
     </>
   );
 }
+
